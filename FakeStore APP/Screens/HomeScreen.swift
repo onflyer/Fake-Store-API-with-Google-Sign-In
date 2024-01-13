23ae462631f30c6b3151fde377e1232f28af23b4
onflyer <salkaorama@gmail.com>
2024-01-13 14:19:41 +0100

    Update HomeScreen.swift

--- FakeStore APP/Screens/HomeScreen.swift
+++ FakeStore APP/Screens/HomeScreen.swift
@@ -11,6 +11,16 @@ struct HomeScreen: View {
     var body: some View {
         VStack {
             Text("Home screen")
+            
+            Button("Sign out") {
+                
+            }
+            .foregroundStyle(.white)
+                .frame(width: 200, height: 56)
+                .background(Color.green)
+                .clipShape(RoundedRectangle(cornerRadius: 12))
+              
+            
         }
         .padding()
     }
