36d681a473fab6e927d99038f0a9bfb91e5e9a60
onflyer <salkaorama@gmail.com>
2024-01-13 11:47:38 +0100

    Initial Commit

---
+++ FakeStore APP/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FakeStore APP
+//
+//  Created by Aleksandar Milidrag on 1/13/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FakeStore APP/FakeStore_APPApp.swift
@@ -0,0 +1,17 @@
+//
+//  FakeStore_APPApp.swift
+//  FakeStore APP
+//
+//  Created by Aleksandar Milidrag on 1/13/24.
+//
+
+import SwiftUI
+
+@main
+struct FakeStore_APPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
