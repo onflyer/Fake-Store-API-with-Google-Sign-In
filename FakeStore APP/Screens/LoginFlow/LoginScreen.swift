29a5bd48234b5eff931db7d2010bfae1e25e6644
onflyer <salkaorama@gmail.com>
2024-01-22 20:27:48 +0100

    added progressview

--- FakeStore APP/Screens/LoginFlow/LoginScreen.swift
+++ FakeStore APP/Screens/LoginFlow/LoginScreen.swift
@@ -19,60 +19,71 @@ struct LoginScreen: View {
 //    @State private var hasError: Bool = false
     var body: some View {
         
+        ZStack {
         
-            Spacer()
-            VStack(alignment: .center, spacing: 10) {
-                    Text("Welcome")
-                    .font(.system(size: 40, weight: .heavy, design: .rounded))
-                    .foregroundStyle(.black.opacity(0.3))
-                    Image("logo")
-                        .padding(.bottom)
-                    
-                CustomTextField(text: $manager.login.email, placeholder: "Enter your email", hasError: $manager.hasError )
-                    .overlay {
-                        RoundedRectangle(cornerRadius: 12)
-                            .stroke(manager.hasError ? .red.opacity(0.6) : .black.opacity(0.0), lineWidth: 2)
-                        if manager.hasError {
-                            Text("Email cannot be empty and must be valid email")
-                                .foregroundStyle(.red.opacity(0.6))
-                                .font(.footnote)
-                                .offset(x: -81, y: 37)
-                        }
-                    }
-                        .padding(.horizontal)
-                    
-                CustomSecureField(text: $manager.login.password, isSecure: $manager.isSecure, hasError: $manager.hasError, placeholder: "Enter your password")
-                        .padding(.horizontal)
-                
-                Button("Login") {
-                    manager.validateLoginEmail()
-                    manager.validateLoginPassword()
-                    
-                    print(manager.login)
-                    if !manager.hasError {
-                        isRegistering = true
-                        Task {
-                            try await manager.login()
-                            isRegistering = false
-                            guard !manager.hasError else {
-                                return
-                            }
-                            session.signIn()
-                        }
+        Spacer()
+        VStack(alignment: .center, spacing: 10) {
+            Text("Welcome")
+                .font(.system(size: 40, weight: .heavy, design: .rounded))
+                .foregroundStyle(.black.opacity(0.3))
+            Image("logo")
+                .padding(.bottom)
+            
+            CustomTextField(text: $manager.login.email, placeholder: "Enter your email", hasError: $manager.hasError )
+                .overlay {
+                    RoundedRectangle(cornerRadius: 12)
+                        .stroke(manager.hasError ? .red.opacity(0.6) : .black.opacity(0.0), lineWidth: 2)
+                    if manager.hasError {
+                        Text("Email cannot be empty and must be valid email")
+                            .foregroundStyle(.red.opacity(0.6))
+                            .font(.footnote)
+                            .offset(x: -81, y: 37)
                     }
                 }
-                .foregroundStyle(.white)
-                .frame(width: 200, height: 56)
-                .background(Color.green)
-                .clipShape(RoundedRectangle(cornerRadius: 12))
-                .padding()
-            }
-            Spacer()
+                .padding(.horizontal)
             
-      
+            CustomSecureField(text: $manager.login.password, isSecure: $manager.isSecure, hasError: $manager.hasError, placeholder: "Enter your password")
+                .padding(.horizontal)
+            
+            Button("Login") {
+                manager.validateLoginEmail()
+                manager.validateLoginPassword()
+                
+                print(manager.login)
+                if !manager.hasError {
+                    isRegistering = true
+                    Task {
+                        try await manager.login()
+                        isRegistering = false
+                        guard !manager.hasError else {
+                            return
+                        }
+                        session.signIn()
+                    }
+                }
+            }
+            .foregroundStyle(.white)
+            .frame(width: 200, height: 56)
+            .background(Color.green)
+            .clipShape(RoundedRectangle(cornerRadius: 12))
+            .padding()
+        }
+        Spacer()
         
         
         
+        
+    }
+        .overlay {
+            if isRegistering {
+                Color.black.opacity(0.4).ignoresSafeArea()
+                ProgressView()
+                    .tint(.white)
+            }
+        }
+        .animation(.easeInOut, value: isRegistering)
+        .ignoresSafeArea()
+
 
     }
 }
